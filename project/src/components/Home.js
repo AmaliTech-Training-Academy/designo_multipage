c0124271ee13158e05efded43196389d757aa1a9
BlissArmah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2022-12-05 15:18:46 +0000

    structure

---
+++ project/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+  return (
+   <div className='home'>
+    Hello guys, all the best
+   </div>
+  )
+}
+
+export default Home
