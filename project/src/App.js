84525fafbb514da7584f909b154cbc8f85ee97b4
MichaelAffanyi <michael.affanyi@amalitech.com>
2022-12-06 16:11:42 +0000

    Add structure

--- project/src/App.js
+++ project/src/App.js
@@ -1,16 +1,15 @@
-
-import Footer from "./components/Footer/Footer";
-import Design from "./components/App_Design/Design";
-// import pattern from "./assets/app-design/desktop/bg-pattern-intro-app.svg"
+import React from "react";
 import "./index.css"
 import './App.css'
 import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="app"  >
-      <Header />
-      <Design/>
-      <Footer/>
-
+    <>
+      <div className="App"  >
+        <Header />
+      </div>
+    </>
+  )
+  }
 export default App;
---
+++ project/src/components/Home/HomeStyles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const HomeContainer = styled.div`
+    width: 100%;
+    height: 640px;
+    background: #E7816B;
+`
+export const Article = styled.div`
+`
+export const ArticleImage = styled.div`
+`
