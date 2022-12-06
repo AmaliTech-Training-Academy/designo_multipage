2d462869ea10a5ec9d83a677d92591d3dedce8d4
MichaelAffanyi <michael.affanyi@amalitech.com>
2022-12-06 17:33:16 +0000

    Add Home

---
+++ project/src/components/Home/HomeStyles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+import Styled from 'styled-components'
+
+export const HomeContainer = styled.div`
+    width: 100%;
+    height: 640px;
+    background: #E7816B;
+`
+export const Article = styled.div`
+`
+export const ArticleImage = styled.div`
+`
