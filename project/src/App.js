777ec9f34beb4640a45402db7ad20945de29a53f
aboagyewaa1 <ewurama.amponsah@amalitech.com>
2022-12-06 13:58:59 +0000

    top part

--- project/src/App.js
+++ project/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Header from './components/Header/Header';
 import Home from "./components/Home/Home";
+import TopAbout from './components/About/top';
 
 function App() {
   return (
     <div className='App'>
-      <Header />
+      <Header/>
       <Home/>
+      <TopAbout/>
+    
     </div>
   );
 }
---
+++ project/src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+import '../'
+
+
+function aboutPage(){
+    return (
+        <>
+        </>
+    );
+}
+
+
+export default aboutPage;
