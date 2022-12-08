b2953c70cbef9c04f718b1dd9ffc0fb35eb79d4a
aboagyewaa1 <ewurama.amponsah@amalitech.com>
2022-12-08 13:39:54 +0000

    creating location component

--- project/src/components/Location/LocationCall.js
+++ project/src/components/Location/LocationCall.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Location from "./location";
+import './location.css'
 // images
 import CanadaMap from "../../assets/locations/desktop/image-map-canada.png"
 import AustraliaMap from "../../assets/locations/desktop/image-map-australia.png"
@@ -13,14 +14,42 @@ import UKTablet from "../../assets/locations/tablet/image-map-uk.png"
 
 const LocationCall=() => {
     return(
-        <>
+        <div className="loc-page-main">
         <Location
         title="Canada"
+        titleA= "Designo  Central Office"
+        subtitle= "3886 Wellington Street"
+        other="Toronto, Ontario M9C 3J5"
+        phone=""
         sizes="(min-width: 1090px) 341px, (min-width: 650px) 40vw,  50vw"
         srcSet={`${CanadaTablet} 689w, ${CanadaMap} 375w`}
         src={CanadaMap}
+        phone="P : +1 253-863-8967"
+        mail="M : contact@designo.co"
+
         />
-        </>
+         <Location
+          title="Canada"
+          titleA= "Designo  Central Office"
+          subtitle= "3886 Wellington Street"
+          other="Toronto, Ontario M9C 3J5"
+          phone=""
+        src={AustraliaMap}
+        title="Australia"
+        sizes="(min-width: 1090px) 341px, (min-width: 650px) 40vw,  50vw"
+        srcSet={`${AustraliaTablet} 689w, ${AustraliaMap} 375w`} />
+         
+         <Location
+          title="Canada"
+          titleA= "Designo  Central Office"
+          subtitle= "3886 Wellington Street"
+          other="Toronto, Ontario M9C 3J5"
+          phone=""
+        src={UKMap}
+        title="United Kingdom"
+        sizes="(min-width: 1090px) 341px, (min-width: 650px) 40vw,  50vw"
+        srcSet={`${UKTablet} 689w, ${UKMap} 375w`} />
+        </div>
 
     );
 
