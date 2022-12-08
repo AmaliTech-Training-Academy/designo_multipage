520e663658f5ee9c81108f530ace945d0bc214d2
aboagyewaa1 <ewurama.amponsah@amalitech.com>
2022-12-08 15:50:02 +0000

    mobile view location page

--- project/src/components/Location/LocationCall.js
+++ project/src/components/Location/LocationCall.js
@@ -20,33 +20,33 @@ const LocationCall=() => {
         titleA= "Designo  Central Office"
         subtitle= "3886 Wellington Street"
         other="Toronto, Ontario M9C 3J5"
-        phone=""
+        phone="P : +1 253-863-8967"
+        mail="M : contact@designo.co"
         sizes="(min-width: 1090px) 341px, (min-width: 650px) 40vw,  50vw"
         srcSet={`${CanadaTablet} 689w, ${CanadaMap} 375w`}
         src={CanadaMap}
-        phone="P : +1 253-863-8967"
-        mail="M : contact@designo.co"
+        
 
         />
          <Location
-          title="Canada"
-          titleA= "Designo  Central Office"
-          subtitle= "3886 Wellington Street"
-          other="Toronto, Ontario M9C 3J5"
-          phone=""
+          title="Australia"
+          titleA= "Designo AU Office"
+          subtitle= "19 Balonne Street"
+          other="New South Wales 2443"
+          phone="P : (02) 6720 9092"
+          mail="M : contact@designo.au"
         src={AustraliaMap}
-        title="Australia"
         sizes="(min-width: 1090px) 341px, (min-width: 650px) 40vw,  50vw"
         srcSet={`${AustraliaTablet} 689w, ${AustraliaMap} 375w`} />
          
          <Location
-          title="Canada"
-          titleA= "Designo  Central Office"
-          subtitle= "3886 Wellington Street"
-          other="Toronto, Ontario M9C 3J5"
-          phone=""
+          title="United Kingdom"
+          titleA= "Designo UK Office"
+          subtitle= "13  Colorado Way"
+          other="Rhyd-y-fro SA8 9GA"
+          phone="P : 078 3115 1400"
+          mail="M : contact@designo.uk"
         src={UKMap}
-        title="United Kingdom"
         sizes="(min-width: 1090px) 341px, (min-width: 650px) 40vw,  50vw"
         srcSet={`${UKTablet} 689w, ${UKMap} 375w`} />
         </div>
