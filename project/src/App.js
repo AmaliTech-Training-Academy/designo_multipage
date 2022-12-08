dfcf08307791c5477e55b3e368bc457469fab78e
aboagyewaa1 <ewurama.amponsah@amalitech.com>
2022-12-08 09:42:08 +0000

    css for location components

--- project/src/App.js
+++ project/src/App.js
@@ -3,13 +3,14 @@ import "./index.css"
 import './App.css'
 import Header from './components/Header/Header';
 import TopAbout from "./components/About/top";
-
+import Location from "./components/Location/location";
 
 function App() {
   return (
     <>
       <div className="App"  >
         <Header />
+        <Location/>
        
       </div>
     </>
