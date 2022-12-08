bb17aad32a3ca4d9a249dfa63077c41153179862
MichaelAffanyi <michael.affanyi@amalitech.com>
2022-12-08 14:28:28 +0000

    Add home component

--- project/src/App.js
+++ project/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./index.css"
 import './App.css'
 import Header from './components/Header/Header';
-import TopAbout from "./components/About/top";
+import Home from "./components/Home/Home";
 
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
     <>
       <div className="App"  >
         <Header />
-       
+        <Home />     
       </div>
     </>
   )
