9f384833b7f37b9fb95b0bfec95443dfa989bf3e
aboagyewaa1 <ewurama.amponsah@amalitech.com>
2022-12-08 13:39:54 +0000

    creating location component

--- project/src/components/Location/location.js
+++ project/src/components/Location/location.js
@@ -5,11 +5,27 @@ const Location = (props) => {
     return(
         <div className="loc-page">
          <div className="loc-big-div">
-          <div className="loc-text">
-                <h3>{props.title}</h3>
+          <div className="loc-text-1">
+            <div className="loc-holder">
+            <h3 className="loc-title">{props.title}</h3>
+                <div className="loc-text-row">
+                   <div className="sub-loc-1" id="loc-right">
+                    <h4 className="loc-small-text " id="loc-small-title">{props.titleA}</h4>
+                    <h4 className="loc-small-text" id="loc-normal-text">{props.subtitle}</h4>
+                    <h4 className="loc-small-text" id="loc-normal-text">{props.other}</h4>
+                    </div>
+                    <div className="sub-loc-1">
+                    <h4 className="loc-small-text " id="loc-small-title">Contact</h4>
+                    <h4 className="loc-small-text" id="loc-normal-text">{props.phone}</h4>
+                    <h4 className="loc-small-text" id="loc-normal-text">{props.mail}</h4>
+                    </div>
+                </div>
+
+            </div>
+              
           </div>
           <div className="loc-img">
-          <img src={props.src}
+          <img id="img-loc-1"src={props.src}
                sizes={props.sizes}
               srcSet={props.srcSet}
                alt={props.alt} />
