9824188cbdaefba44c935b416b7c56a46c7a7d63
BlissArmah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2022-12-11 12:28:14 +0000

    styling contact page

--- project/src/components/Contact/contact.js
+++ project/src/components/Contact/contact.js
@@ -15,7 +15,7 @@ const Contact = () => {
   }
   const handleSubmit = (e) =>{
     e.preventDefault()
-    setFormErrors(validate(values))
+      setFormErrors(validate(values))
     setIsSubmit(true)
   }
 
@@ -41,26 +41,29 @@ const Contact = () => {
   return (
 
     <div className='contact-page'>
+      
+      <div className='svg-container'>
+
+      <svg width="640" height="640" xmlns="http://www.w3.org/2000/svg"><defs><linearGradient x1="0%" y1="50%" x2="100%" y2="50%" id="a"><stop stop-color="#5D0202" stop-opacity="0" offset="0%"/><stop stop-color="#5D0202" stop-opacity=".498" offset="100%"/></linearGradient></defs><circle fill="url(#a)" transform="matrix(0 1 1 0 0 0)" cx="320" cy="320" r="320" fill-rule="evenodd" opacity=".309"/></svg>
+      </div>
       <div className='contact'>
         <h1>Contact Us</h1>
-        <p className='text'>bhbhjbjhbhbkbkjbkjbbjkbjb,b,mbnbbbbbkjbjbjbjbkjbjbm, m,nmnn,nnjnjkbjkbkjbjbjklkbjbhbhbbhjb bbjbhbhbhjbjhbjbhjbhjbhjbjhvgvgvgvb bbjbhbhbhjbjhbjbhjbhjbhjbjhvgvgvgvhgvjhg</p>
+        <div className='text'>
+        Ready to take it to the next level? Let's talk about your project or idea and find out how we can help your business grow. If you are looking for unique digital experiences that's relatable to your users, drop us a line. 
+        </div>
       </div>
       <form onSubmit={handleSubmit}>
         <input type='text' name="username" placeholder='Name' value={values.username} onChange={handleChange} />
         <p className='error'>{formErrors.username}</p>
-        <hr></hr>
         <input type='email' name="email" placeholder='Email Address' value={values.email} onChange={handleChange} />
         <p className='error'>{formErrors.email}</p>
 
-        <hr></hr>
         <input type='tel' name="phone" placeholder='Phone' value={values.phone} onChange={handleChange} />
         <p className='error'>{formErrors.phone}</p>
         
-        <hr></hr>
         <textarea type='text' name="message" placeholder='Your Message' value={values.message} onChange={handleChange}/>
         <p className='error'>{formErrors.message}</p>
 
-        <hr></hr>
         <Button type="submit" className='btn'>Submit</Button>
 
       </form>
