40a2ab79e718f23c9e36a27fbff46265b36f551c
aboagyewaa1 <ewurama.amponsah@amalitech.com>
2022-12-13 10:48:50 +0000

    resolving merge conflicts

--- project/src/App.js
+++ project/src/App.js
@@ -2,18 +2,29 @@ import React from "react";
 import "./index.css"
 import './App.css'
 import Header from './components/Header/Header';
+<<<<<<< HEAD
 import Home from "./pages/Home/Home";
 import WebDesign from "./pages/Web_Design/WebDesign";
 // import MobileNavMenu from "./components/Header/MobileNavMenu";
 
+=======
+import TopAbout from "./components/About/top";
+import Location from "./components/Location/location";
+>>>>>>> dfcf083 (css for location components)
 
 function App() {
   return (
     <>
       <div className="App"  >
+<<<<<<< HEAD
   
         <WebDesign />
 
+=======
+        <Header />
+        <Location/>
+       
+>>>>>>> dfcf083 (css for location components)
       </div>
     </>
   )
---
+++ project/src/components/Location/location.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './location.css'
+
+const Location = (props) => {
+    return(
+        <div className="loc-page">
+         <div className="loc-big-div">
+          <div className="loc-text">
+hjfkjhfjfk
+          </div>
+          <div className="loc-img">
+
+          </div>
+         </div>
+        </div>
+
+    );
+}
+
+
+export default Location
