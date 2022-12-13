97dd5f884bbcd8f92c97de7377e64915328ab994
Bliss  Armah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2022-12-13 09:54:50 +0000

    react router setup

--- project/src/App.js
+++ project/src/App.js
@@ -1,22 +1,16 @@
-
-import Footer from "./components/Footer/Footer";
-import Design from "./components/App_Design/Design";
-// import pattern from "./assets/app-design/desktop/bg-pattern-intro-app.svg"
 import "./index.css"
 import './App.css'
 import Header from './components/Header/Header';
-import Graphic from "./components/Graphic_Design/Graphic";
-import Contact from "./components/Contact/contact";
-import {BrowserRoute} from "react-router-dom"
-import Home from "./components/App_Design/Design";
+import {BrowserRouter} from "react-router-dom"
+import AppRoutes from "./routes";
 
 function App() {
   return (
     <div className="App"  >
-      <Header />
-      <Contact/>
-      {/* <Graphic/> */}
-      {/* <Home/> */}
+      <BrowserRouter>
+      <Header/>
+      <AppRoutes/>
+      </BrowserRouter>
       </div>
   )}
 export default App;
