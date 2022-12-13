0a284ac3422795d08ae79de2f718b65243f45959
aboagyewaa1 <ewurama.amponsah@amalitech.com>
2022-12-13 10:50:32 +0000

    resolving conflicts

--- project/src/components/Location/location.js
+++ project/src/components/Location/location.js
@@ -6,9 +6,13 @@ const Location = (props) => {
         <div className="loc-page">
          <div className="loc-big-div">
           <div className="loc-text">
-hjfkjhfjfk
+                <h3>{props.title}</h3>
           </div>
           <div className="loc-img">
+          <img src={props.src}
+               sizes={props.sizes}
+              srcSet={props.srcSet}
+               alt={props.alt} />
 
           </div>
          </div>
