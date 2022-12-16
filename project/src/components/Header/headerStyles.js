1b211751d3510bbf7ff458c20be599ce71552fb3
Bliss  Armah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2022-12-16 11:25:28 +0000

    additional styles

--- project/src/components/Header/headerStyles.js
+++ project/src/components/Header/headerStyles.js
@@ -3,18 +3,39 @@ import styled from 'styled-components'
 export const Nav = styled.nav`
     width: 100%;
     height: 30px;
-    margin-top: 64px;
+    margin-top: ${(props) => props.footer === 'footer' ? '0' : '64px'};
+    margin-bottom: ${(props) => props.footer === 'footer' ? '40px' : '0'};
     display: flex;
     justify-content: space-between;
 
     @media (max-width: 449px) {
         width: 327px;
         margin: 64px auto 0;
+        flex-direction: ${(props) => props.footer === 'footer' ? 'column' : 'unset'};
+        gap: ${(props) => props.footer === 'footer' ? '32px' : 'unset'};
+    }
+
+    a {
+        @media (max-width: 450px) {
+            display: flex;
+            justify-content: ${(props) => props.footer === 'footer' ? 'center' : 'unset'};
+        }
     }
 `
 export const Logo = styled.img`
     width: 196px;
     cursor: pointer;
+    content: url(${(props) => props.footer === 'footer' ? "/assets/shared/desktop/logo-light.png" : "/assets/shared/desktop/logo-dark.png"});
+`
+export const Line = styled.div`
+    height: 2px;
+    width: 100%;
+    background: #ffffff;
+    opacity: 0.1;
+    display: none;
+    @media (max-width: 449px) {
+        display: ${(props) => props.footer === 'footer' ? 'block' : 'none'};
+    }
 `
 export const NavLinks = styled.div`
     text-transform: uppercase;
@@ -25,7 +46,7 @@ export const NavLinks = styled.div`
     
     a{
         position: relative;
-        color: #333136;
+        color: ${(props) => props.footer === 'footer' ? '#ffffff' : '#333136'};
         text-decoration: none;
         cursor: pointer;
 
@@ -33,7 +54,7 @@ export const NavLinks = styled.div`
             content: "";
             height: 1px;
             width: 100%;
-            background: rgb(51, 49, 54);
+            background: ${(props) => props.footer === 'footer' ? '#ffffff' : '#333136'};
             position: absolute;
             left: 0;
             right: 0;
@@ -49,13 +70,19 @@ export const NavLinks = styled.div`
                 display: block;
             }
         }
-        @media (max-width: 450px) {
-            display: none;
+        @media (max-width: 449px) {
+            display: ${(props) => props.footer === 'footer' ? 'block' : 'none'};
         }
     }
 
     @media (min-width: 451px) and (max-width: 950px) {
         width: 370px;
+    }
+
+    @media (max-width: 449px) {
+        flex-direction: ${(props) => props.footer === 'footer' ? 'column' : 'unset'};
+        width: ${(props) => props.footer === 'footer' ? '100%' : '40%'};
+        gap: ${(props) => props.footer === 'footer' ? '30px' : 'unset'};
     }
 `
 export const Bars = styled.div`
@@ -71,8 +98,11 @@ export const Bars = styled.div`
     }
 
     @media (max-width: 450px) {
-        display: block;
+        display: ${(props) => props.footer === 'footer' ? 'none' : 'block'};
     }
 `
 export const Close = styled(Bars)`
+    @media (max-width: 450px) {
+        display: ${(props) => props.footer === 'footer' ? 'none' : 'block'};
+    }
 `
