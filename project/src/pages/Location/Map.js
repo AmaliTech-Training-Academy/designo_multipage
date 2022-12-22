ea5c90c0305a170b6b935605fcb826707619c1d6
aboagyewaa1 <ewurama.amponsah@amalitech.com>
2022-12-22 11:33:10 +0000

    changes

--- project/src/pages/Location/Map.js
+++ project/src/pages/Location/Map.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { MapContainer, TileLayer,Marker } from 'react-leaflet'
 import markerIconPng from "leaflet/dist/images/marker-icon.png"
 import {Icon} from 'leaflet'
@@ -9,7 +9,7 @@ const mapImage = (lat, lng) => {
   return(
     <MapContainer center={[lat, lng]} zoom={13} scrollWheelZoom={false}>
     <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright"></a> '
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
     <Marker position={[lat, lng]} icon={new Icon({iconUrl: markerIconPng, iconSize: [25, 41]})}>
